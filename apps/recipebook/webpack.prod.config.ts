904b912712a6a90f408fec4fbf90b7f082255d4f
Mark Hiserodt <markhiserodt@cypherfusion.com>
2024-01-14 09:01:26 -0800

    Remote static web app module grab

--- apps/recipebook/webpack.prod.config.ts
+++ apps/recipebook/webpack.prod.config.ts
@@ -3,7 +3,4 @@ import config from './module-federation.config';
 
 export default withModuleFederation({
   ...config,
-  remotes: [
-    ['inventory', 'https://zealous-sea-02c6b701e.4.azurestaticapps.net']
-  ]
 });
