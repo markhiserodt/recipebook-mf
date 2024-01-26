db28eca643911b74ff9a1e1a6a0bd1b2da5ab483
markhiserodt-avanade <mark.hiserodt@avanade.com>
2024-01-25 16:13:32 -0800

    Updated account msal redirect

--- apps/account/src/environments/environment.prod.ts
+++ apps/account/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   msal: {
     clientId: 'f6b112f2-375a-41c0-b27f-aae04c58a072',
     tenantId: 'f66b9a1c-1fb5-4639-822e-f61383bf4e2b',
-    redirect: 'https://zealous-sea-02c6b701e.4.azurestaticapps.net'
+    redirect: 'https://thankful-sky-09bb07f1e.4.azurestaticapps.net'
   },
   graph: {
       uri: 'https://graph.microsoft.com/v1.0/me',
