9b99ff3936761a822a347838e6f5be2650ae15a0
markhiserodt-avanade <mark.hiserodt@avanade.com>
2024-01-25 16:07:57 -0800

    CI/CD integrated with Account and MSAL

--- apps/account/src/environments/environment.prod.ts
+++ apps/account/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://recipebook-inventory-server.azurewebsites.net/'
+  apiUrl: 'https://recipebook-inventory-server.azurewebsites.net/',
+  msal: {
+    clientId: 'f6b112f2-375a-41c0-b27f-aae04c58a072',
+    tenantId: 'f66b9a1c-1fb5-4639-822e-f61383bf4e2b',
+    redirect: 'https://zealous-sea-02c6b701e.4.azurestaticapps.net'
+  },
+  graph: {
+      uri: 'https://graph.microsoft.com/v1.0/me',
+      scopes: ['user.read']
+  }
 }
--- apps/recipebook/src/environments/environment.prod.ts
+++ apps/recipebook/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   msal: {
     clientId: 'f6b112f2-375a-41c0-b27f-aae04c58a072',
     tenantId: 'f66b9a1c-1fb5-4639-822e-f61383bf4e2b',
-    redirect: 'http://localhost:4200'
+    redirect: 'https://ashy-meadow-0a0b8611e.4.azurestaticapps.net'
   },
   graph: {
       uri: 'https://graph.microsoft.com/v1.0/me',
